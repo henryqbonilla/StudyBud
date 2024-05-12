dd805314acb585dd058d9b4ab3ed9bdfa6c4a83b
Henry X1 <henryqbonilla@gmail.com>
2024-05-12 00:35:17 -0400

    change rooms to database

--- base/views.py
+++ base/views.py
@@ -1,23 +1,14 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': "Lets Learn Python!"},
-    {'id': 2, 'name': "Design with me"},
-    {'id': 3, 'name': "Frontend Developers"},
-]
-
-# This is a comment i added on the X1, commited and pushed to github
-
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
